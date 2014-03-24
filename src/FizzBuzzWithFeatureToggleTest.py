8060fd8be4f999801bb9c8f985450a868fa366b4
Ben Wu <wubin28@gmail.com>
2014-03-24 09:14:01 +0800

    Extracted tests for common numbers to test_when_common_number_then_say_it_directly().

--- src/FizzBuzzWithFeatureToggleTest.py
+++ src/FizzBuzzWithFeatureToggleTest.py
@@ -4,11 +4,12 @@ from FizzBuzzWithFeatureToggle import FizzBuzzWithFeatureToggle
 
 
 class FizzBuzzWithFeatureToggleTest(unittest.TestCase):
-    def test_when_3_then_Fizz(self):
+    def test_when_common_number_then_say_it_directly(self):
         self.assertEqual(1, FizzBuzzWithFeatureToggle().countOff(1))
         self.assertEqual(2, FizzBuzzWithFeatureToggle().countOff(2))
-        self.assertEqual('Fizz', FizzBuzzWithFeatureToggle().countOff(3))
 
+    def test_when_3_then_Fizz(self):
+        self.assertEqual('Fizz', FizzBuzzWithFeatureToggle().countOff(3))
 
 if __name__ == "__main__":
     unittest.main()
