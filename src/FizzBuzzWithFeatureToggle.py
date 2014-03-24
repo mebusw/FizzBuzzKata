4bb84cb2d19ac37d4b019d0a6102a7f352db4dce
Ben Wu <wubin28@gmail.com>
2014-03-24 09:29:46 +0800

    Renamed class FizzBuzzWithFeatureToggle to be FizzBuzzWithBodyMovement.

--- src/FizzBuzzWithFeatureToggle.py
+++ src/FizzBuzzWithFeatureToggle.py
@@ -1,4 +1,4 @@
-class FizzBuzzWithFeatureToggle():
+class FizzBuzzWithBodyMovement():
     def countOff(self, n):
         if (n == 3):
             return 'Fizz'
--- src/FizzBuzzWithFeatureToggleTest.py
+++ src/FizzBuzzWithFeatureToggleTest.py
@@ -1,15 +1,16 @@
 import unittest
 
-from FizzBuzzWithFeatureToggle import FizzBuzzWithFeatureToggle
+from FizzBuzzWithFeatureToggle import FizzBuzzWithBodyMovement
 
 
 class FizzBuzzWithFeatureToggleTest(unittest.TestCase):
     def test_when_common_number_then_say_it_directly(self):
-        self.assertEqual(1, FizzBuzzWithFeatureToggle().countOff(1))
-        self.assertEqual(2, FizzBuzzWithFeatureToggle().countOff(2))
+        self.assertEqual(1, FizzBuzzWithBodyMovement().countOff(1))
+        self.assertEqual(2, FizzBuzzWithBodyMovement().countOff(2))
 
     def test_when_3_then_Fizz(self):
-        self.assertEqual('Fizz', FizzBuzzWithFeatureToggle().countOff(3))
+        self.assertEqual('Fizz', FizzBuzzWithBodyMovement().countOff(3))
+
 
 if __name__ == "__main__":
     unittest.main()
