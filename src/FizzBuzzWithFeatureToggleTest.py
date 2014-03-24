2d1596c552634e102f3c54230f54931a6a29cdfa
Ben Wu <wubin28@gmail.com>
2014-03-24 09:38:53 +0800

    Added test_when_5_then_Buzz().

--- src/FizzBuzzWithFeatureToggleTest.py
+++ src/FizzBuzzWithFeatureToggleTest.py
@@ -11,6 +11,9 @@ class FizzBuzzWithFeatureToggleTest(unittest.TestCase):
     def test_when_3_then_Fizz(self):
         self.assertEqual('Fizz', FizzBuzz().countOff(3))
 
+    def test_when_5_then_Buzz(self):
+        self.assertEqual('Buzz', FizzBuzz().countOff(5))
+
 
 if __name__ == "__main__":
     unittest.main()
