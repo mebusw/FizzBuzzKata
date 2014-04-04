de7da1505100f1a1eac64fb6d4631d719e21d50b
Ben Wu <wubin28@gmail.com>
2014-04-04 12:44:23 +0800

    Made equivalence class partitioning (selecting the mininum, medium and maximum numbers) for the tests (assuming the max number is 100).

--- src/FizzBuzzWithFeatureToggleTest.py
+++ src/FizzBuzzWithFeatureToggleTest.py
@@ -8,19 +8,24 @@ class FizzBuzzTestBodyMovementToggleOffTest(unittest.TestCase):
 
     def test_given_body_movement_is_off_when_common_number_then_say_it_directly(self):
         self.assertEqual(1, FizzBuzz().countOff(1))
-        self.assertEqual(2, FizzBuzz().countOff(2))
+        self.assertEqual(49, FizzBuzz().countOff(49))
+        self.assertEqual(98, FizzBuzz().countOff(98))
 
     def test_given_body_movement_is_off_when_multiple_of_3_then_Fizz(self):
         self.assertEqual('Fizz', FizzBuzz().countOff(3))
-        self.assertEqual('Fizz', FizzBuzz().countOff(6))
+        self.assertEqual('Fizz', FizzBuzz().countOff(51))
+        self.assertEqual('Fizz', FizzBuzz().countOff(99))
 
     def test_given_body_movement_is_off_when_multiple_of_5_then_Buzz(self):
         self.assertEqual('Buzz', FizzBuzz().countOff(5))
-        self.assertEqual('Buzz', FizzBuzz().countOff(10))
+        self.assertEqual('Buzz', FizzBuzz().countOff(50))
+        self.assertEqual('Buzz', FizzBuzz().countOff(100))
 
     def test_given_body_movement_is_off_when_multiple_of_3_or_5_then_FizzBuzz(self):
         self.assertEqual('FizzBuzz', FizzBuzz().countOff(15))
-        self.assertEqual('FizzBuzz', FizzBuzz().countOff(30))
+        self.assertEqual('FizzBuzz', FizzBuzz().countOff(45))
+        self.assertEqual('FizzBuzz', FizzBuzz().countOff(90))
+
 
 class FizzBuzzBodyMovementToggleOnTest(unittest.TestCase):
     def setUp(self):
