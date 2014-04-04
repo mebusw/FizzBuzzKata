8c727ac8616211d40c8fd7ef043fd868299824d9
Ben Wu <wubin28@gmail.com>
2014-04-04 12:19:55 +0800

    Made test_when_multiple_of_3_or_5_then_FizzBuzz() passed.

--- src/FizzBuzzWithFeatureToggle.py
+++ src/FizzBuzzWithFeatureToggle.py
@@ -3,6 +3,7 @@ class FizzBuzz():
     featureToggleBodyMovement = False
     
     def countOff(self, n):
+        if (n % 15 == 0): return FizzBuzz.featureToggleBodyMovement and ' ' or 'FizzBuzz'
         if (n % 3 == 0): return FizzBuzz.featureToggleBodyMovement and 'FizzAndTouchHead' or 'Fizz'
         if (n % 5 == 0): return FizzBuzz.featureToggleBodyMovement and 'BuzzAndTouchShoulder' or 'Buzz'
         return n
