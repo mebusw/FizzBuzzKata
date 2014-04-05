0234043ec3f3ee90b8a080331e08ceaf2d4e29c8
josephyao <dermott@126.com>
2014-04-05 21:52:58 +0800

    refactor to remove some duplication and add some domain concept

--- src/FizzBuzzWithFeatureToggle.py
+++ src/FizzBuzzWithFeatureToggle.py
@@ -3,7 +3,16 @@ class FizzBuzz():
     featureToggleBodyMovement = False
     
     def countOff(self, n):
-        if (n % 15 == 0): return 'FizzBuzz'
-        if (n % 3 == 0): return FizzBuzz.featureToggleBodyMovement and 'FizzAndTouchHead' or 'Fizz'
-        if (n % 5 == 0): return FizzBuzz.featureToggleBodyMovement and 'BuzzAndTouchShoulder' or 'Buzz'
+        if (self.isFizz(n) and self.isBuzz(n)): 
+        	return 'FizzBuzz'
+        if (self.isFizz(n)): 
+        	return self.featureToggleBodyMovement and 'FizzAndTouchHead' or 'Fizz'
+        if (self.isBuzz(n)):
+        	return self.featureToggleBodyMovement and 'BuzzAndTouchShoulder' or 'Buzz'
         return n
+
+    def isFizz(self, n):
+    	return n % 3 == 0
+
+    def isBuzz(self, n):
+    	return n % 5 == 0
