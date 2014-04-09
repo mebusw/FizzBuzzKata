a6ca7bbeb271c17ee5f5998b9eeaec6438d2333f
josephyao <dermott@126.com>
2014-04-09 22:57:35 +0800

    move text code to two classes so that FizzBuzz can decide which one to use by feature toggle

--- src/FizzBuzzWithFeatureToggle.py
+++ src/FizzBuzzWithFeatureToggle.py
@@ -1,12 +1,25 @@
+class FizzBuzzText():
+
+	def fizz_text(self):
+		return 'Fizz'
+
+class FizzBuzzBodyMovementText():
+
+	def fizz_text(self):
+		return 'FizzAndTouchHead'
+
 class FizzBuzz():
 	
     feature_toggle_body_movement = False
-    
+ 
+    def create_text(self):
+    	return FizzBuzzBodyMovementText() if self.feature_toggle_body_movement else FizzBuzzText()
+
     def count_off(self, n):
         if self.is_fizz(n) and self.is_buzz(n): 
         	return self.fizzbuzz_text()
         if self.is_fizz(n): 
-        	return self.fizz_text()
+        	return self.create_text().fizz_text()
         if self.is_buzz(n):
         	return self.buzz_text()
         return n
@@ -16,12 +29,6 @@ class FizzBuzz():
 
     def is_buzz(self, n):
     	return n % 5 == 0
-
-    def fizz_text(self):
-    	if self.feature_toggle_body_movement:
-    		return 'FizzAndTouchHead'
-    	else:
-    		return 'Fizz'
 
     def buzz_text(self):
     	if self.feature_toggle_body_movement:
@@ -34,3 +41,4 @@ class FizzBuzz():
     		return 'FizzBuzzAndTouchKnee'
     	else:
     		return 'FizzBuzz'
+
