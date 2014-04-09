db39cfdb33169cdd7b4a9d0a688719312ea389ba
josephyao <dermott@126.com>
2014-04-09 23:01:12 +0800

    refactor the code for buzz text

--- src/FizzBuzzWithFeatureToggle.py
+++ src/FizzBuzzWithFeatureToggle.py
@@ -3,10 +3,16 @@ class FizzBuzzText():
 	def fizz_text(self):
 		return 'Fizz'
 
+	def buzz_text(self):
+		return 'Buzz'
+
 class FizzBuzzBodyMovementText():
 
 	def fizz_text(self):
 		return 'FizzAndTouchHead'
+
+	def buzz_text(self):
+		return 'BuzzAndTouchShoulder'
 
 class FizzBuzz():
 	
@@ -16,12 +22,14 @@ class FizzBuzz():
     	return FizzBuzzBodyMovementText() if self.feature_toggle_body_movement else FizzBuzzText()
 
     def count_off(self, n):
+    	text = self.create_text()
+
         if self.is_fizz(n) and self.is_buzz(n): 
         	return self.fizzbuzz_text()
         if self.is_fizz(n): 
-        	return self.create_text().fizz_text()
+        	return text.fizz_text()
         if self.is_buzz(n):
-        	return self.buzz_text()
+        	return text.buzz_text()
         return n
 
     def is_fizz(self, n):
@@ -29,12 +37,6 @@ class FizzBuzz():
 
     def is_buzz(self, n):
     	return n % 5 == 0
-
-    def buzz_text(self):
-    	if self.feature_toggle_body_movement:
-    		return 'BuzzAndTouchShoulder'
-    	else:
-    		return 'Buzz'
 
     def fizzbuzz_text(self):
     	if self.feature_toggle_body_movement:
