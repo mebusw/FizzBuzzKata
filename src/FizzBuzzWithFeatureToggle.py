b3bbba7eeea25c09b6e2fe4fc9626ef0cf5da962
Ben Wu <wubin28@gmail.com>
2014-04-18 08:25:14 +0800

    Replaced tabs with spaces in file FizzBuzzWithFeatureToggle.py.

--- src/FizzBuzzWithFeatureToggle.py
+++ src/FizzBuzzWithFeatureToggle.py
@@ -1,45 +1,45 @@
 class FizzBuzzText():
 
-	def fizz_text(self):
-		return 'Fizz'
+    def fizz_text(self):
+        return 'Fizz'
 
-	def buzz_text(self):
-		return 'Buzz'
+    def buzz_text(self):
+        return 'Buzz'
 
-	def fizzbuzz_text(self):
-		return 'FizzBuzz'
+    def fizzbuzz_text(self):
+        return 'FizzBuzz'
 
 class FizzBuzzBodyMovementText():
 
-	def fizz_text(self):
-		return 'FizzAndTouchHead'
+    def fizz_text(self):
+        return 'FizzAndTouchHead'
 
-	def buzz_text(self):
-		return 'BuzzAndTouchShoulder'
+    def buzz_text(self):
+        return 'BuzzAndTouchShoulder'
 
-	def fizzbuzz_text(self):
-		return 'FizzBuzzAndTouchKnee'
+    def fizzbuzz_text(self):
+        return 'FizzBuzzAndTouchKnee'
 
 class FizzBuzz():
-	
+    
     feature_toggle_body_movement = False
  
     def create_text_generator(self):
-    	return FizzBuzzBodyMovementText() if self.feature_toggle_body_movement else FizzBuzzText()
+        return FizzBuzzBodyMovementText() if self.feature_toggle_body_movement else FizzBuzzText()
 
     def count_off(self, n):
-    	text_generator = self.create_text_generator()
+        text_generator = self.create_text_generator()
 
         if self.is_fizz(n) and self.is_buzz(n): 
-        	return text_generator.fizzbuzz_text()
+            return text_generator.fizzbuzz_text()
         if self.is_fizz(n): 
-        	return text_generator.fizz_text()
+            return text_generator.fizz_text()
         if self.is_buzz(n):
-        	return text_generator.buzz_text()
+            return text_generator.buzz_text()
         return n
 
     def is_fizz(self, n):
-    	return n % 3 == 0
+        return n % 3 == 0
 
     def is_buzz(self, n):
-    	return n % 5 == 0
+        return n % 5 == 0
